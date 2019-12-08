2eeefb0a13f7e2e9b979b2c525206884ecf9a5c7
oelliots <olgaschou@Olgas-MacBook-Pro.local>
2019-12-08 23:51:37 +0100

    add review_texts

--- data/review_texts.js
+++ data/review_texts.js
@@ -1,1 +1,9 @@
-var review_texts = [];
+var review_texts = [  "Wauw, wat was dit mooi. Ik raad iedereen aan om naar boven te gaan via de trap en verder te wandelen. Daar zijn nog zo veel meer mooie watervallen, die stiekem nog mooier zijn.",
+  "Mooie waterval, vanaf de N1 al te zien, Voldoende parkeergelegenheid.",
+  "Gigantisch indrukwekkende waterval. Je kan hem al zie vanaf de weg maar hij is indrukwekkender vanaf dichtbij. Er is een grote parking en volgens mij ook een café. Een regenjas is aangeraden omdat je dan dichterbij kan komen. Als de zon schijnt ontstaat er een dubbele regenboog die prachtig is om foto's mee te maken.",
+  "Veel toerisme en veel mooier dan op de foto's",
+  "Niet de mooiste waterval van IJsland, maar wel een aanrader! Jammer alleen dat er zoveel toeristen voor staan, maar ja, daar hoorden we zelf ook bij :)",
+  "Mooie waterval je kan hier ook naar boven lopen",
+  "Echt mooie grote waterval velen trappen lopen voor zicht van boven af",
+  "Deze waterval is mooi, met de zon erbij krijg je een regenboog!"
+];
